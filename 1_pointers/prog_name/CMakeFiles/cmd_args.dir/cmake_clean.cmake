32ac08e3c6e5475009d1ef02295f1b8afb88cd74
olegovich22 <gool10@ukr.net>
2019-10-14 16:26:57 +0300

    prog_name program

---
+++ 1_pointers/prog_name/CMakeFiles/cmd_args.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmd_args.dir/main.c.o"
+  "cmd_args.pdb"
+  "cmd_args"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cmd_args.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
